a6b6dd0d645115e31276fdc8a55656d56f909f54
f.clyde@inov.cm <f.clyde@inov.cm>
2023-09-26 10:58:18 +0000

    Created ServerModule1, ArticleEdit and Home; Edited settings; Deleted Form1

---
+++ client_code/ArticleEdit/__init__.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import ArticleEditTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class ArticleEdit(ArticleEditTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+    # Any code you write here will run before the form opens.
+    self.categories = [
+      (cat['name'], cat) for cat in app_tables.categories.search()
+    ]
+    self.category_box.items = self.categories
+
+  def image_uploader_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    # Add the image to self.item
+    self.item['image'] = file
+
---
+++ client_code/Home/__init__.py
@@ -0,0 +1,30 @@
+from ._anvil_designer import HomeTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from ..ArticleEdit import ArticleEdit
+
+class Home(HomeTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def add_article_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Initialise an empty dictionary to store the user inputs
+    new_article = {}
+    # Open an alert displaying the 'ArticleEdit' Form
+    save_clicked = alert(
+      content=ArticleEdit(item=new_article),
+      title="Add Article",
+      large=True,
+      buttons=[("Save", True), ("Cancel", False)],
+    )
+    # If the alert returned 'True', the save button was clicked.
+    if save_clicked:
+      print(new_article)
+
